e0feff3aab8da2138c99ddb828783e88f863355b
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-05 16:07:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
                 <?php
                 if($pagina > 1) {
                     //echo "<a href='index.php?pagina=".($pagina - 1)."' class='controle'>&laquo; anterior</a>";
-                    echo "<a href='index.php?pagina=".($pagina - 1)."' class='controle'><button class='button1'>CICLOS ANTERIORES</button1></a>"; 
+                    echo "<a href='index.php?pagina=".($pagina - 1)."' class='controle'><button>CICLOS ANTERIORES</button></a>"; 
                 }
     
                 for($i = 1; $i < $numPaginas; $i++) {
@@ -66,7 +66,7 @@
     
                 if($pagina < $numPaginas) {
                     //echo "<a href='index.php?pagina=".($pagina + 1)."' class='controle'>proximo &raquo;</a>";
-                    echo "<a href='index.php?pagina=".($pagina - 1)."' class='controle'><button class='button1'>EXIBIR PRÓXIMOS CICLOS</button1></a>";
+                    echo "<a href='index.php?pagina=".($pagina - 1)."' class='controle'><button>EXIBIR PRÓXIMOS CICLOS</button></a>";
                 }
                 mysqli_close($conn);
                 ?>
