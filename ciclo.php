23dc2a49f2b58164d35855288ca9e977d0dce9e3
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-05 14:00:11 -0300

    Update ciclo.php

--- ciclo.php
+++ ciclo.php
@@ -21,22 +21,55 @@
             <?php
             include "conexao.php";
 
-            echo'<table width="88%" height="10" cellspacing="0" cellpadding="0"><tr>';
-            
-            $stmt = $conn->prepare("SELECT * FROM `Ciclos` ORDER BY `id` ASC ");
-            $stmt->execute( );
-            $linha = $stmt->fetch(PDO::FETCH_ASSOC);
-            while($linha = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo "<b>Ciclo:"; 
-                ?>
-                <a href="NovoCiclo.php?codigo<?php echo $row["Codigo"]; ?>">
-                <?php
-                echo "</b>" . $row["Titulo-ciclo-aberto"]."</a>";
-                echo "<b> - Data Inicial:</b> ".$row["Data-inicio-geral"];
-                echo "<b> - Data Final: </b>".$row["Data-fim-geral"]."<br><br>";
+            // Informações da query
+            $campos_query = "*";
+            $final_query  = "FROM Ciclos ORDER BY cod ASC";
+            // Maximo de registros por pagina
+            $maximo = 5;
+            // Declaração da pagina inicial
+            $pagina = $_GET["pagina"];
+            if($pagina == "") {
+                $pagina = "1";
             }
-            echo "</tr></table>";
-
+            // Calculando o registro inicial
+            $inicio = $pagina - 1;
+            $inicio = $maximo * $inicio;
+            // Conta os resultados no total da query
+            $strCount = "SELECT COUNT(*) AS 'num_registros' $final_query";
+            $query = mysqli_query($strCount);
+            $row = mysqli_fetch_array($query);
+            $total = $row["num_registros"];
+            ###################################################################################
+            // INICIO DO CONTEÚDO
+            // Realizamos a query
+            $sql = mysqli_query("SELECT $campos_query $final_query LIMIT $inicio,$maximo");
+            // Exibimos os nomes dos produtos e seus repectivos valores
+            while ($linha = mysqli_fetch_object($sql)) {
+                echo "<b>" . $linha->nome . "</b> (R$ ". $linha->valor.")<br />";
+            }
+            // FIM DO CONTEUDO
+            ###################################################################################
+            $menos = $pagina - 1;
+            $mais = $pagina + 1;
+            $pgs = ceil($total / $maximo);
+            if($pgs > 1 ) {
+                echo "<br />";
+                // Mostragem de pagina
+                if($menos > 0) {
+                    echo "<a href=".$_SERVER['PHP_SELF']."?pagina=$menos>anterior</a>  ";
+                }
+                // Listando as paginas
+                for($i=1;$i <= $pgs;$i++) {
+                    if($i != $pagina) {
+                        echo " <a href=".$_SERVER['PHP_SELF']."?pagina=".($i).">$i</a> | ";
+                    } else {
+                        echo " <strong>".$i."</strong> | ";
+                    }
+                }
+                if($mais <= $pgs) {
+                    echo " <a href=".$_SERVER['PHP_SELF']."?pagina=$mais>próxima</a>";
+                }
+            }
 
             mysqli_close($conn);
             ?>
