b18b4436bc3c99ae2e6b905645ba3f4f19362889
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-06 18:02:41 -0300

    Update UpdateCiclo.php

--- UpdateCiclo.php
+++ UpdateCiclo.php
@@ -13,35 +13,29 @@
             <p class='titulo'>CADASTRO ATUALIZADO</p>
             <?php
             $CODIGO = $_POST['codigo'];
-            $data-inicio-geral = $_POST['data-inicio-geral'];
-            $hora-inicio-geral = $_POST['hora-inicio-geral'];
-            $data-fim-geral = $_POST['data-fim-geral'];
-            $hora-fim-geral = $_POST['hora-fim-geral'];
-            $data-inicio-produtores = $_POST['data-inicio-produtores'];
-            $data-fim-produtores = $_POST['data-fim-produtores'];
-            $data-inicio-consumidores = $_POST['data-inicio-consumidores'];
-            $data-fim-consumidores = $_POST['data-fim-consumidores'];
-            $titulo-ciclo-aberto = $_POST['titulo-ciclo-aberto'];
-            $texto-complementar-título = $_POST['texto-complementar-título'];
-            $texto-dicas = $_POST['texto-dicas'];
-            $texto-ciclo-fechado = $_POST['texto-ciclo-fechado'];
-            $url-produtores = $_POST['url-produtores'];
-            $texto-confirma-ofertas= $_POST['texto-confirma-ofertas'];
-            $texto-confirma-pedido-produtores = $_POST['texto-confirma-pedido-produtores'];
-            $url-consumidores = $_POST['url-consumidores'];
-            $texto-confirma-pedido-consumidores = $_POST['texto-confirma-pedido-consumidores'];
-            $url-administradores = $_POST['url-administradores'];
-            $situacao = $_POST['situacao'];
+           // $data-inicio-geral = $_POST['data-inicio-geral'];
+//            $hora-inicio-geral = $_POST['hora-inicio-geral'];
+//            $data-fim-geral = $_POST['data-fim-geral'];
+//            $hora-fim-geral = $_POST['hora-fim-geral'];
+//            $data-inicio-produtores = $_POST['data-inicio-produtores'];
+//            $data-fim-produtores = $_POST['data-fim-produtores'];
+//            $data-inicio-consumidores = $_POST['data-inicio-consumidores'];
+//            $data-fim-consumidores = $_POST['data-fim-consumidores'];
+//            $titulo-ciclo-aberto = $_POST['titulo-ciclo-aberto'];
+//            $texto-complementar-título = $_POST['texto-complementar-título'];
+//            $texto-dicas = $_POST['texto-dicas'];
+//            $texto-ciclo-fechado = $_POST['texto-ciclo-fechado'];
+//            $url-produtores = $_POST['url-produtores'];
+//            $texto-confirma-ofertas= $_POST['texto-confirma-ofertas'];
+//            $texto-confirma-pedido-produtores = $_POST['texto-confirma-pedido-produtores'];
+//            $url-consumidores = $_POST['url-consumidores'];
+//            $texto-confirma-pedido-consumidores = $_POST['texto-confirma-pedido-consumidores'];
+//            $url-administradores = $_POST['url-administradores'];
+//            $situacao = $_POST['situacao'];
            
             include "conexao.php";
             
-            $banco = mysqli_query($conn, "UPDATE Ciclos SET 
-            Data-inicio-geral='$data-inicio-geral', Hora-inicio-gera='$hora-inicio-geral', Data-fim-geral='$data-fim-geral', Hora-fim-geral='$hora-fim-geral',
-            Data-inicio-produtores='$data-inicio-produtores', Data-fim-produtores='$data-fim-produtores', Data-inicio-consumidores='$data-inicio-consumidores',
-            Data-fim-consumidores='$data-fim-consumidores', Titulo-ciclo-aberto='$titulo-ciclo-aberto', Texto-complementar-título='$texto-complementar-título',
-            Texto-dicas='$texto-dicas', Texto-ciclo-fechado='$texto-ciclo-fechado', URL-produtores='$url-produtores', Texto-confirma-ofertas='$texto-confirma-ofertas',
-            Texto-confirma-pedido-produtores='$texto-confirma-pedido-produtores', URL-consumidores='$url-consumidores', Texto-confirma-pedido-consumidores='$texto-confirma-pedido-consumidores',
-            URL-administradores='$url-administradores', Situacao='$situacao' WHERE codigo='$CODIGO'"); 
+            //$banco = mysqli_query($conn, "UPDATE Ciclos SET Data-inicio-geral='$data-inicio-geral', Hora-inicio-gera='$hora-inicio-geral', Data-fim-geral='$data-fim-geral', Hora-fim-geral='$hora-fim-geral', Data-inicio-produtores='$data-inicio-produtores', Data-fim-produtores='$data-fim-produtores', Data-inicio-consumidores='$data-inicio-consumidores', Data-fim-consumidores='$data-fim-consumidores', Titulo-ciclo-aberto='$titulo-ciclo-aberto', Texto-complementar-título='$texto-complementar-título', Texto-dicas='$texto-dicas', Texto-ciclo-fechado='$texto-ciclo-fechado', URL-produtores='$url-produtores', Texto-confirma-ofertas='$texto-confirma-ofertas', Texto-confirma-pedido-produtores='$texto-confirma-pedido-produtores', URL-consumidores='$url-consumidores', Texto-confirma-pedido-consumidores='$texto-confirma-pedido-consumidores', URL-administradores='$url-administradores', Situacao='$situacao' WHERE codigo='$CODIGO'"); 
 
             } 
             ?>
