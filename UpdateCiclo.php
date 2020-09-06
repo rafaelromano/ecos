e33ddc1735e0b7cc8f46d08920b5ae089af7e3ea
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-06 18:30:47 -0300

    Update UpdateCiclo.php

--- UpdateCiclo.php
+++ UpdateCiclo.php
@@ -13,25 +13,45 @@
             <p class='titulo'>CADASTRO ATUALIZADO</p>
             <?php
             $CODIGO = $_POST['codigo'];
+            echo $CODIGO."<br>";
             $datainiciogeral = $_POST['data-inicio-geral'];
+            echo $datainiciogeral."<br>";
             $horainiciogeral = $_POST['hora-inicio-geral'];
+            echo $horainiciogeral."<br>";
             $datafimgeral = $_POST['data-fim-geral'];
+            echo $datafimgeral."<br>";
             $horafimgeral = $_POST['hora-fim-geral'];
+            echo $horafimgeral."<br>";
             $datainicioprodutores = $_POST['data-inicio-produtores'];
+            echo $datainicioprodutores."<br>";
             $datafimprodutores = $_POST['data-fim-produtores'];
+            echo $datafimprodutores."<br>";
             $datainicioconsumidores = $_POST['data-inicio-consumidores'];
+            echo $datainicioconsumidores."<br>";
             $datafimconsumidores = $_POST['data-fim-consumidores'];
+            echo $datafimconsumidores."<br>";
             $titulocicloaberto = $_POST['titulo-ciclo-aberto'];
+            echo $titulocicloaberto."<br>";
             $textocomplementartítulo = $_POST['texto-complementar-título'];
+            echo $textocomplementartítulo."<br>";
             $textodicas = $_POST['texto-dicas'];
+            echo $textodicas."<br>";
             $textociclofechado = $_POST['texto-ciclo-fechado'];
+            echo $textociclofechado."<br>";
             $urlprodutores = $_POST['url-produtores'];
+            echo $urlprodutores."<br>";
             $textoconfirmaofertas= $_POST['texto-confirma-ofertas'];
+            echo $textoconfirmaofertas."<br>";
             $textoconfirmapedidoprodutores = $_POST['texto-confirma-pedido-produtores'];
+            echo $textoconfirmapedidoprodutores."<br>";
             $urlconsumidores = $_POST['url-consumidores'];
+            echo $urlconsumidores."<br>";
             $textoconfirmapedidoconsumidores = $_POST['texto-confirma-pedido-consumidores'];
+            echo $textoconfirmapedidoconsumidores."<br>";
             $urladministradores = $_POST['url-administradores'];
+            echo $urladministradores."<br>";
             $situacao = $_POST['situacao'];
+            echo $situacao."<br>";
            
             include "conexao.php";
             
@@ -49,7 +69,14 @@
             ?>
         </td>
     </tr>
-    
+    <tr>
+            <td>
+                <br>
+                <?php
+                echo "<a href='index.php'><button class='button3'>PÁGINA INICIAL</button></a>";
+                ?>
+            </td>
+    </tr>
 </table>
 
 </center>
