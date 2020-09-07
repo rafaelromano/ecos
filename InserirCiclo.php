6cb1ce1707131c22954885d92b81182395d972b5
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-07 16:30:31 -0300

    Update InserirCiclo.php

--- InserirCiclo.php
+++ InserirCiclo.php
@@ -55,15 +55,17 @@
            
             include "conexao.php";
             
-            mysqli_query($conn, "INSET INTO `Ciclos` (`Codigo`, `Data-inicio-geral`, `Hora-inicio-geral`, `Data-fim-geral`,
-             `Hora-fim-geral`, `Data-inicio-produtores`, `Data-fim-produtores`, `Data-inicio-consumidores`, `Data-fim-consumidores`,
-             `Titulo-ciclo-aberto`, `Texto-complementar-título`, `Texto-dicas`, `Texto-ciclo-fechado`, `URL-produtores`,
-             `Texto-confirma-ofertas`, `Texto-confirma-pedido-produtores`, `URL-consumidores`, `Texto-confirma-pedido-consumidores`,
-             `URL-administradores`, `Situacao`) VALEUS 
-             (NULL,'$datainiciogeral','$horainiciogeral','$datafimgeral','$horafimgeral','$datainicioprodutores','$datafimprodutores',
-            '$datainicioconsumidores','$datafimconsumidores','$titulocicloaberto','$textocomplementartítulo','$textodicas',
-            '$textociclofechado','$urlprodutores','$textoconfirmaofertas','$textoconfirmapedidoprodutores','$urlconsumidores',
-            '$textoconfirmapedidoconsumidores','$urladministradores','$situacao')");
+            $sql=("INSET INTO `Ciclos` (`Codigo`, `Data-inicio-geral`, `Hora-inicio-geral`, `Data-fim-geral`,
+            `Hora-fim-geral`, `Data-inicio-produtores`, `Data-fim-produtores`, `Data-inicio-consumidores`, `Data-fim-consumidores`,
+            `Titulo-ciclo-aberto`, `Texto-complementar-título`, `Texto-dicas`, `Texto-ciclo-fechado`, `URL-produtores`,
+            `Texto-confirma-ofertas`, `Texto-confirma-pedido-produtores`, `URL-consumidores`, `Texto-confirma-pedido-consumidores`,
+            `URL-administradores`, `Situacao`) VALEUS 
+            (NULL,'$datainiciogeral','$horainiciogeral','$datafimgeral','$horafimgeral','$datainicioprodutores','$datafimprodutores',
+           '$datainicioconsumidores','$datafimconsumidores','$titulocicloaberto','$textocomplementartítulo','$textodicas',
+           '$textociclofechado','$urlprodutores','$textoconfirmaofertas','$textoconfirmapedidoprodutores','$urlconsumidores',
+           '$textoconfirmapedidoconsumidores','$urladministradores','$situacao')");
+            
+            $res= mysqli_query($conn,$sql); 
             
             mysqli_close($conn);
             ?>
