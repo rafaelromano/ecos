445320f7bfa5741dc618b91eda5f07720a4fd704
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-07 16:39:49 -0300

    Update InserirCiclo.php

--- InserirCiclo.php
+++ InserirCiclo.php
@@ -59,7 +59,7 @@
             `Hora-fim-geral`, `Data-inicio-produtores`, `Data-fim-produtores`, `Data-inicio-consumidores`, `Data-fim-consumidores`,
             `Titulo-ciclo-aberto`, `Texto-complementar-título`, `Texto-dicas`, `Texto-ciclo-fechado`, `URL-produtores`,
             `Texto-confirma-ofertas`, `Texto-confirma-pedido-produtores`, `URL-consumidores`, `Texto-confirma-pedido-consumidores`,
-            `URL-administradores`, `Situacao`) VALEUS 
+            `URL-administradores`, `Situacao`) VALUES 
             (NULL,'$datainiciogeral','$horainiciogeral','$datafimgeral','$horafimgeral','$datainicioprodutores','$datafimprodutores',
            '$datainicioconsumidores','$datafimconsumidores','$titulocicloaberto','$textocomplementartítulo','$textodicas',
            '$textociclofechado','$urlprodutores','$textoconfirmaofertas','$textoconfirmapedidoprodutores','$urlconsumidores',
