1b4e90712503eadf885d09053865e08b3aa83ed8
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-19 11:28:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,9 @@
 
             $inicio = ($registros*$pagina)-$registros; 
 
-            $banco = mysqli_query($conn, "SELECT * FROM Ciclos WHERE Situacao='1' ORDER BY 'Data-inicio-geral' ASC
+            //$banco = mysqli_query($conn, "SELECT * FROM Ciclos WHERE Situacao='1' ORDER BY 'Data-inicio-geral' ASC");
+            
+            $banco = mysqli_query($conn, "SELECT * FROM `Ciclos` WHERE Situacao='1' ORDER BY `Data-inicio-geral` ASC");
             LIMIT $inicio,$registros"); 
             $total = mysqli_num_rows($banco); 
 
