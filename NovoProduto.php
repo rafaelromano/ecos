7aaaf37e2f8af37e068c99c7ef2ac2c75e2462dd
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-20 14:47:05 -0300

    Create NovoProduto.php

---
+++ NovoProduto.php
@@ -0,0 +1,133 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Produtos</title>
+        <script>
+            function validar()
+                {
+                    //ar datainiciogeral = document.getElementById("data-inicio-geral");
+                    var datainiciogeral = formulario.data-inicio-geral.value;
+                    var horainiciogeral = document.getElementById("hora-inicio-geral");
+                    var datafimgeral = document.getElementById("data-fim-geral");
+                    var horafimgeral = document.getElementById("hora-fim-geral");
+                    var datainicioprodutores = document.getElementById("data-inicio-produtores");
+                    var datafimprodutores = document.getElementById("data-fim-produtores");
+                    var datainicioconsumidores = document.getElementById("data-inicio-consumidores");
+                    var datafimconsumidores = document.getElementById("data-fim-consumidores");
+                    var titulocicloaberto = document.getElementById("titulo-ciclo-aberto");
+                    var textocomplementartítulo = document.getElementById("texto-complementar-título");
+                    var textodicas = document.getElementById("texto-dicas");
+                    var textociclofechado = document.getElementById("texto-ciclo-fechado");
+                    var urlprodutores = document.getElementById("url-produtores");
+                    var textoconfirmaofertas= document.getElementById("texto-confirma-ofertas");
+                    var textoconfirmapedidoprodutores = document.getElementById("texto-confirma-pedido-produtores");
+                    var urlconsumidores = document.getElementById("url-consumidores");
+                    var textoconfirmapedidoconsumidores = document.getElementById("texto-confirma-pedido-consumidores");
+                    var urladministradores = document.getElementById("url-administradores");
+                    var situacao = document.getElementById("situacao");
+
+                    if(datainiciogeral == " " || datainiciogeral == NULL)
+                        {
+                            alert("Data Inicial do Clico não definida!");
+                            formulario.data-inicio-geral.value.focus();
+                            return false                           // se inválida :(
+                        }
+                    
+                }
+        </script>
+</head>
+
+<body>
+<center>
+<p class='titulo1'>Feira Orgânico Solidário </p>
+<table class='table'>
+    <tr>
+        <td class='td01'>
+            <p class='titulo'>NOVO PRODUTO</p>
+            <?php
+            $CODIGO = $_GET['codigo'];
+            if($CODIGO<>"")
+                {
+                ?>
+                <form method="post" action="UpdateProduto.php" name="formulario">
+                <?php
+                }
+            else
+                {
+                ?>
+                <form method="post" action="InserirProdutp.php" name="formulario">
+                <?php
+                }
+            if($CODIGO<>"")
+            {
+                include "conexao.php";              
+                $banco = mysqli_query($conn, "SELECT * FROM Produtos WHERE Codigo='".$CODIGO."'"); 
+                $total = mysqli_num_rows($banco); 
+                while($exibe_ciclos = mysqli_fetch_array($banco)) { 
+                    echo "<p class='texto'><b>Código:</b><input type='hidden' name='codigo' value='".$exibe_produtos["Codigo"]."'>".$exibe_produtos["Codigo"]."</p>";
+                    echo "<p class='texto'><b>Código Produtor:</b><br><input type='date' class='texto' size='10' maxlength='10' name='pt-codigo' value='".$exibe_produtos["PT-Codigo"]."'></p>";
+                    echo "<p class='texto'><b>Código Categoria:</b><br><input type='time' class='texto' size='5'  maxlength='5' name='cp-codigo' value='".$exibe_produtos["CP-Codigo"]."'></p>";
+                    echo "<p class='texto'><b>Descição:</b><br><input type='date' class='texto' size='20'  maxlength='200' name='descricao' value='".$exibe_produtos["Descricao"]."'></p>";
+                    echo "<p class='texto'><b>:</b><br><input type='time' class='texto' size='5'  maxlength='10' name='qtd-disponivel' value='".$exibe_produtos["Qtd-Disponivel"]."'></p>";
+                    echo "<p class='texto'><b>:</b><br><input type='time' class='texto' size='5'  maxlength='10' name='custo' value='".$exibe_produtos["Custo"]."'></p>";
+                    echo "<p class='texto'><b>:</b><br><input type='time' class='texto' size='5'  maxlength='10' name='preco' value='".$exibe_produtos["Preco"]."'></p>";
+                    if($exibe_produtos["Disponibilidade"] == 1)
+                        {
+                        $selecao1="checked";
+                        }
+                    else
+                        {
+                        $selecao2="checked";
+                        }
+                    
+                    echo "<p class='texto'><b>Disponibilidade:</b><br><input type='radio' name='disponibilidade' value='1' ".$selecao1.">Aberto - <input type='radio' name='disponibilidade' value='0' ".$selecao2.">Fechado<br></p>";      
+                    } 
+            }
+            else
+            { 
+                $banco1 = mysqli_query($conn, "SELECT Codido,Descricao FROM Produtores"); 
+                $total = mysqli_num_rows($banco1); 
+                echo "<p class='texto'><b>Código Produtor:</b><br><selec name='pt-codigo'><option>Selecione...</option>";
+                while($exibe_produtores = mysqli_fetch_array($banco1)) { 
+                    echo "<option value='".$exibe_produtores["Codigo"].">".$exibe_produtores["Descicao"]."</option></p>";
+                }
+
+                $banco2 = mysqli_query($conn, "SELECT Codido,Descricao FROM Produtores"); 
+                $total = mysqli_num_rows($banco2); 
+                echo "<p class='texto'><b>Código Produtor:</b><br><selec name='cp-codigo'><option>Selecione...</option>";
+                while($exibe_categoria = mysqli_fetch_array($banco2)) { 
+                    echo "<option value='".$exibe_categoria["Codigo"].">".$exibe_categoria["Descicao"]."</option></p>";
+                }
+                echo "<p class='texto'><b>Descição:</b><br><input type='date' class='texto' size='200'  maxlength='20' name='descricao'></p>";
+                echo "<p class='texto'><b>:</b><br><input type='time' class='texto' size='5'  maxlength='10' name='qtd-disponivel'></p>";
+                echo "<p class='texto'><b>:</b><br><input type='time' class='texto' size='5'  maxlength='10' name='custo'></p>";
+                echo "<p class='texto'><b>:</b><br><input type='time' class='texto' size='5'  maxlength='10' name='preco'></p>";             
+                echo "<p class='texto'><b>Disponibilidade:</b><br><input type='radio' name='disponibilidade' value='1'>SIM - <input type='radio' name='disponibilidade' value='0'>NÃO<br></p>";      
+            }
+            ?>
+        </td>
+    </tr>
+    <tr>
+            <td>
+                <br>
+                <?php
+                if($CODIGO<>"")
+                {
+                    echo "<input type='submit' class='button3' value='ATUALIZAR' onclick='return validar()'></form>";
+                }
+                else
+                {
+                    echo "<input type='submit' class='button3' value='CADASTRAR' onclick='return validar()'></form>";
+                }
+                echo "&nbsp;&nbsp;&nbsp;<a href='produtos.php'><button class='button3'>CANCELAR</button></a>";
+                
+                mysqli_close($conn);
+                ?>
+            </td>
+    </tr>
+</table>
+
+</center>
+</body>
+</html>
