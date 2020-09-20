0a883e8389cf9b71dd7ba9ef5d58a0715c365f57
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-20 15:43:17 -0300

    Update NovoProduto.php

--- NovoProduto.php
+++ NovoProduto.php
@@ -88,15 +88,15 @@
             else
             { 
                 $banco1 = mysqli_query($conn, "SELECT * FROM Produtores"); 
-                $total = mysqli_num_rows($banco1); 
+                //$total = mysqli_num_rows($banco1); 
                 echo "<p class='texto'><b>Código Produtor:</b><br><select name='pt-codigo'><option value='0'>Selecione...</option>";
                 while($exibe_produtores = mysqli_fetch_array($banco1)) { 
                     echo "<option value='".$exibe_produtores["Codigo"]."'>".$exibe_produtores["Codigo"]." - ".$exibe_produtores["Nome"]."</option></p>";
                 }
                 echo "</select>";
 
-                $banco2 = mysqli_query($conn, "SELECT * FROM 'Categoria-Produtos'"); 
-                $total = mysqli_num_rows($banco2); 
+                $banco2 = mysqli_query($conn, "SELECT * FROM Categoria-Produtos"); 
+                //$total = mysqli_num_rows($banco2); 
                 echo "<p class='texto'><b>Categoria:</b><br><select name='cp-codigo'><option value='0'>Selecione...</option>";
                 while($exibe_categoria = mysqli_fetch_array($banco2)) { 
                     echo "<option value='".$exibe_categoria["Codigo"]."'>".$exibe_categoria["Codigo"]." - ".$exibe_categoria["Descicao"]."</option></p>";
