eab4959b521ab422ac574215501a0a4867ba9bf1
rafaelromanosilveira <silveira.romano@gmail.com>
2020-09-20 16:54:31 -0300

    Update InserirProduto.php

--- InserirProduto.php
+++ InserirProduto.php
@@ -16,15 +16,15 @@
             <?php
            
             $ptcodigo = $_POST['pt-codigo'];
-            echo $ptcodigo."<br>";
+            //echo $ptcodigo."<br>";
             $cpcodigo = $_POST['cp-codigo'];
-            echo $cpcodigo."<br>";
+            //echo $cpcodigo."<br>";
             $descricao = $_POST['descricao'];
-            echo $descricao."<br>";
+            //echo $descricao."<br>";
             $custo = $_POST['custo'];
-            echo $custo."<br>";
+            //echo $custo."<br>";
             $preco = $_POST['preco'];
-            echo $preco."<br>";
+            //echo $preco."<br>";
             $disponibilidade = $_POST['disponibilidade'];
             
            
@@ -37,7 +37,7 @@
             mysqli_close($conn);
             ?>
             <br>
-            <!-- <div align="center" class="titulo">Você será redirecionado em: <br><div classs="titulo1" id="timers">3 segundos</div>-->
+            <div align="center" class="titulo">Você será redirecionado em: <br><div classs="titulo1" id="timers">3 segundos</div>
         </td>
     </tr>
     <tr>
